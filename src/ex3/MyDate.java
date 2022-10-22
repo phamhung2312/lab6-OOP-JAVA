a6333a939f37d5f56c025b5ff926d3091df9c25c
phamhung <phamhung23122003@gmail.com>
2022-10-22 13:35:38 +0700

    fist commit

--- src/ex3/MyDate.java
+++ src/ex3/MyDate.java
@@ -4,21 +4,146 @@ public class MyDate {
     private int year;
     private int month;
     private int day;
-    String[] MONTHS=new String[]{"Jan","Feb","Mar","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-    String[] DAYS = new String[]{"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    int[] DAYS_IN_MONTHS=new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+    String[] MONTHS = new String[]{"Jan", "Feb", "Mar", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    String[] DAYS = new String[]{"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    int[] DAYS_IN_MONTHS = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    public boolean isLeapYear(int year){
-        return (year%4==0&&year%100!=0)||year%400==0?true:false;
+    public boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? true : false;
     }
 
-    public boolean isValidDate(int year,int month,int day){
-        if(year>9999||year<1||month<1||month>12||day<1){
+    public boolean isValidDate(int year, int month, int day) {
+        if (year > 9999 || year < 1 || month < 1 || month > 12 || day < 1) {
             return false;
         }
-        if(isLeapYear(year) && month==2&&day<=29){
+        if (isLeapYear(year) && month == 2 && day <= 29) {
             return true;
         }
-        return day<=DAYS_IN_MONTHS[month-1]?true:false;
+        return day <= DAYS_IN_MONTHS[month - 1] ? true : false;
     }
+
+    public int getDayOfWeek(int year, int month, int day) {
+        int centuryCode = 0;
+        switch ((year / 100) % 4) {
+            case 1 -> centuryCode = 4;
+            case 2 -> centuryCode = 2;
+            case 3 -> centuryCode = 0;
+            case 0 -> centuryCode = 6;
+        }
+        int yearLastTwoDigit = year - (year / 100) * 100;
+        int yearCode = yearLastTwoDigit / 4;
+        int monthCode = 0;
+        switch (month) {
+            case 3, 11 -> monthCode = 3;
+            case 4, 7 -> monthCode = 6;
+            case 5 -> monthCode = 1;
+            case 6 -> monthCode = 4;
+            case 8 -> monthCode = 2;
+            case 9, 12 -> monthCode = 5;
+            case 10 -> monthCode = 0;
+        }
+        if (!isLeapYear(year)) {
+            switch (month) {
+                case 1 -> monthCode = 0;
+                case 2 -> monthCode = 3;
+            }
+        } else {
+            switch (month) {
+                case 1 -> monthCode = 6;
+                case 2 -> monthCode = 2;
+            }
+        }
+        return (centuryCode + yearLastTwoDigit + yearCode + monthCode + day) % 7;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return DAYS[getDayOfWeek(this.year, this.month, this.day)] +
+                " " + day +
+                " " + MONTHS[month - 1] +
+                " " + year;
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public MyDate nextYear() {
+        if(year==9999) throw new IllegalStateException("Year out of range");
+        year++;
+        return this;
+    }
+
+    public MyDate nextMonth() {
+        if(month==12) {
+            month=0;
+            nextYear();
+            return this;
+        }
+        month++;
+        return this;
+    }
+
+    public MyDate nextDay() {
+        if(day==DAYS_IN_MONTHS[month-1]||(isLeapYear(year) && month == 2 && day == 29)) {
+            day=0;
+            nextMonth();
+            return this;
+        }
+        day++;
+        return this;
+    }
+
+    public MyDate previousYear() {
+        if(year==1) throw new IllegalStateException("Year out of range");
+        year--;
+        return this;
+    }
+
+    public MyDate previousMonth() {
+        if(month==0) {
+            month=12;
+            previousYear();
+            return this;
+        }
+        month--;
+        return this;
+    }
+
+    public MyDate previousDay() {
+        if(day==1) {
+            previousMonth();
+            day=(isLeapYear(year) && month == 2)?29:DAYS_IN_MONTHS[month-1];
+            return this;
+        }
+        day--;
+        return this;
+    }
+
 }
