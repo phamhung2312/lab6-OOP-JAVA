8e083a0a0ad1f22bd835669629b3bf4a06ee3261
phamhung <phamhung23122003@gmail.com>
2022-10-24 22:17:28 +0700

    finish

--- src/ex3/MyDate.java
+++ src/ex3/MyDate.java
@@ -143,7 +143,7 @@ public class MyDate {
     }
 
     public void fixValidDate(){
-        if (!isValidDate(year,month,day)){
+        while (!isValidDate(year,month,day)){
             day--;
         }
     }
